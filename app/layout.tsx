b1b94f31dc873b5ea49d69ecd9c3f11cba14a9c0
Ibrahim Faris <ibrahimfaris636@gmail.com>
2026-06-17 10:07:52 +0700

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import Container from "@/components/Container";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +28,10 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased px-10`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <Header />
+      <body className="font-poppins antialiased">{children}</body>
     </html>
   );
 }
---
+++ components/HeaderMenu.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HeaderMenu = () => {
+  return <>HeaderMenu</>;
+};
+
+export default HeaderMenu;
